0d6129ae90747b259f3576faa643ccaa876a3cb5
amousavigourabi <28668597+amousavigourabi@users.noreply.github.com>
2023-08-13 01:35:14 +0200

    Link against a slightly older glibc

--- linux/compile-post-0-9-3.sh
+++ linux/compile-post-0-9-3.sh
@@ -2,5 +2,6 @@
 wget https://archive.apache.org/dist/thrift/$1/thrift-$1.tar.gz
 tar -xvzf thrift-$1.tar.gz
 cd thrift-$1
-./configure --without-java --without-ruby --without-c_glib --without-cpp --without-py3 --without-python --disable-tutorial --disable-libs --disable-tests --disable-fast-install CXXFLAGS="-O3 -Os -Wno-error=type-limits" CFLAGS="-O3 -Os"
+chmod +x configure
+./configure --without-java --without-ruby --without-c_glib --without-cpp --without-py3 --without-python --disable-tutorial --disable-libs --disable-tests --disable-fast-install CFLAGS="-O3 -Os" CXXFLAGS="-O3 -Os -Wno-error=type-limits"
 make install
